4821d1b434a971ef236bc44976c37bec7b18c7c3
Shikha <shikha@mentationgroup.com>
2024-09-05 20:07:54 +0530

    login and resiter page issue solved

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Suspense, useEffect } from 'react'
-import { HashRouter, Route, Routes } from 'react-router-dom'
+// import { HashRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useSelector } from 'react-redux'
 
 import { CSpinner, useColorModes } from '@coreui/react'
@@ -33,26 +34,24 @@ const App = () => {
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <HashRouter>
-      {/* <Suspense
+    <Router>
+      <Suspense
         fallback={
           <div className="pt-3 text-center">
             <CSpinner color="primary" variant="grow" />
           </div>
         }
       >
-
-      </Suspense> */}
-
-      <Routes>
-        <Route exact path="/login" name="Login Page" element={<Login />} />
-        <Route exact path="/register" name="Register Page" element={<Register />} />
-        <Route exact path="/404" name="Page 404" element={<Page404 />} />
-        <Route exact path="/500" name="Page 500" element={<Page500 />} />
-        <Route path="*" name="Home" element={<DefaultLayout />} />
-      </Routes>
-    </HashRouter>
-  )
+        <Routes>
+          <Route exact path="/login" name="Login Page" element={<Login />} />
+          <Route exact path="/register" name="Register Page" element={<Register />} />
+          <Route exact path="/404" name="Page 404" element={<Page404 />} />
+          <Route exact path="/500" name="Page 500" element={<Page500 />} />
+          <Route path="*" name="Home" element={<DefaultLayout />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
 }
 
 export default App
